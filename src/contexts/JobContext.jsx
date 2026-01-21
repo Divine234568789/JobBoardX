8d009951b986d67be012dc494bca6e650564164c
Divine234568789 <anyanwudee@gmail.com>
2026-01-21 22:22:47 +0100

    fixed vercel error

---
+++ src/contexts/JobContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const JobContext = createContext();
+
+export const JobProvider = ({ children }) => {
+  const [jobs, setJobs] = useState(() => {
+    const storedJobs = localStorage.getItem("jobs");
+    return storedJobs ? JSON.parse(storedJobs) : [];
+  });
+
+  const [jobInProgress, setJobInProgress] = useState({});
+
+  // persist jobs
+  useEffect(() => {
+    localStorage.setItem("jobs", JSON.stringify(jobs));
+  }, [jobs]);
+
+  const addJob = (firstJob) => {
+    const jobWithId = {
+      ...firstJob,
+      id: crypto.randomUUID(),
+      postedAt: new Date().toISOString(),
+    };
+
+    setJobs((prevJobs) => [jobWithId, ...prevJobs]);
+    setJobInProgress({});
+  };
+
+  const updateTempJob = (data) => {
+    setJobInProgress((prev) => ({
+      ...prev,
+      ...data,
+    }));
+  };
+
+  return (
+    <JobContext.Provider
+      value={{
+        jobs,
+        addJob,
+        jobInProgress,
+        updateTempJob,
+      }}
+    >
+      {children}
+    </JobContext.Provider>
+  );
+};
+
+export const useJobContext = () => useContext(JobContext);
